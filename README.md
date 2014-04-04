9fc790dfcef607ab2029da5e1ba1ced30ed538c9
Karsten Loesing <karsten.loesing@gmx.net>
2014-04-04 18:56:42 +0200

    Write some code that combines Onionoo documents.

---
+++ README.md
@@ -0,0 +1,45 @@
+Graphs for the Tor Relay Challenge
+==================================
+
+EFF is planning to do another ["Tor relay challenge"] [EFF], to both help raise awareness of the value of Tor, and encourage many people to run relays.
+We should provide them with graphs visualizing progress.
+
+Instructions
+------------
+
+```sh
+python challenge.py
+```
+
+See the TODOs in the Python file to switch from locally stored test files to actually downloading files from the [Onionoo service] [Onionoo].
+
+Output
+------
+
+ - `combined-uptime.json` contains combined uptime fractions of all relays or bridges listed in `fingerprints.txt`.  This graph can be used to display "Total number of relays and bridges".
+ - `combined-bandwidth.json` contains bandwidth histories of all relays or bridges listed in `fingerprints.txt`.  The unit is total bytes per second read or written by all relays and bridges.  This graph shows "Total bandwidth consumed by relays and bridges".
+ - `combined-weights.json` contains sums of advertised bandwidth fractions, consensus weight fractions, and guard/middle/exit probabiliby fractions.  One possible graph would be "Total consensus weight fraction added by relays".
+ - `combined-clients.json` contains average number of clients served by all bridges listed in `fingerprints.txt`.
+
+The data format is pretty similar to the [Onionoo protocol specification] [Onionoo].
+
+Next steps
+----------
+ - Turn this script into a web app with URLs like `http://example.com/combined-uptime/F2044413DAC2E02E3D6BCF4735A19BCA1DE97281+9695DFC35FFEB861329B9F1AB04C46397020CE31
+   - How about using Flask?
+ - Add local cache for downloading documents from Onionoo.
+   - When we cache documents, we should store them together with their `Last-Modified` timestamp.
+   - We also remember the latest `Last-Modified` timestamp that we saw in any request.
+   - When fetching a new set of documents, we include that timestamp in the `If-Modified-Since` header.  If we receive a response with a newer `Last-Modified` timestamp, we flush our entire cache.  If we receive a `304 Not Modified`, we can use all documents from our cache and only have to fetch the ones we don't have.
+   - It's important that we make at least one request to Onionoo, or we might never learn when there's new data.  We can make one such request per request for data, or we can rate-limit that to once every minute or so.
+   - We should probably not limit our cache size, because that would only make us refetch data that we fetched before.  The cache gets cleared after an hour anyway.
+ - Add local cache for results.  We're likely going to be asked for the very same data over and over.
+   - We should cache results together with the `Last-Modified` timestamp of the documents they are based on.
+   - Whenever we learn that Onionoo has new data for us, let's flush our results cache.
+ - Write a website with graphs using our data.  We can probably re-use parts of either Atlas [Atlas] or Globe [Globe] here.
+
+[EFF]:https://www.eff.org/torchallenge
+[Onionoo]:https://onionoo.torproject.org/
+[Atlas]:https://atlas.torproject.org/
+[Globe]:https://globe.torproject.org/
+
---
+++ challenge.py
@@ -0,0 +1,216 @@
+#!/usr/bin/env python
+import urllib2
+import json
+import optparse
+import os
+import sys
+from datetime import timedelta, datetime
+
+
+def main():
+    options = parse_options()
+    fingerprints = read_fingerprints(options.in_fingerprints)
+    download_and_combine_documents(options.out_bandwidth, 'bandwidth',
+                                   fingerprints)
+    download_and_combine_documents(options.out_weights, 'weights',
+                                   fingerprints)
+    download_and_combine_documents(options.out_clients, 'clients',
+                                   fingerprints)
+    download_and_combine_documents(options.out_uptime, 'uptime',
+                                   fingerprints)
+
+def parse_options():
+    parser = optparse.OptionParser()
+    parser.add_option('-f', action='store', dest='in_fingerprints',
+                      default='fingerprints.txt', metavar='FILE',
+                      help='read relay fingerprints and/or hashed bridge '
+                           'fingerprint as input [default: %default]')
+    parser.add_option('-b', action='store', dest='out_bandwidth',
+                      default='combined-bandwidth.json', metavar='FILE',
+                      help='write combined bandwidth document as output '
+                           '[default: %default]')
+    parser.add_option('-w', action='store', dest='out_weights',
+                      default='combined-weights.json', metavar='FILE',
+                      help='write combined weights document as output '
+                           '[default: %default]')
+    parser.add_option('-u', action='store', dest='out_uptime',
+                      default='combined-uptime.json', metavar='FILE',
+                      help='write combined uptime document as output '
+                           '[default: %default]')
+    parser.add_option('-c', action='store', dest='out_clients',
+                      default='combined-clients.json', metavar='FILE',
+                      help='write combined clients document as output '
+                           '[default: %default]')
+    (options, args) = parser.parse_args()
+    return options
+
+def read_fingerprints(fingerprints_path):
+    if not os.path.exists(fingerprints_path):
+        print('Fingerprints file %s does not exist.  Exiting.' % (
+               fingerprints_path, ))
+        sys.exit(1)
+    fingerprints_file = open(fingerprints_path)
+    fingerprints_content = fingerprints_file.read()
+    fingerprints_file.close()
+    fingerprints = []
+    for line in fingerprints_content.split('\n'):
+        if '#' in line:
+            stripped_line = line[:line.index('#')].strip()
+        else:
+            stripped_line = line.strip()
+        if len(stripped_line) == 40:
+            fingerprints.append(stripped_line)
+        elif len(stripped_line) > 0:
+            print('Skipping invalid fingerprint: %s' % (stripped_line, ))
+    return fingerprints
+
+def download_and_combine_documents(out_path, resource_name, fingerprints):
+    # TODO put me in after testing
+    #downloads = download_documents(resource_name, fingerprints)
+    downloads = read_documents_from_disk(resource_name, fingerprints)
+    combined_document = combine_downloads(downloads)
+    write_combined_document_to_disk(out_path, combined_document)
+
+def download_documents(resource_name, fingerprints):
+    downloads = []
+    for fingerprint in fingerprints:
+        request = 'https://onionoo.torproject.org/%s?lookup=%s' % (
+                   resource_name, fingerprint, )
+        try:
+            response = urllib2.urlopen(request)
+        except urllib2.HTTPError, error:
+            print("Error " + str(error.code) + ": " + error.reason)
+        downloads.append(response.read())
+    return downloads
+
+def read_documents_from_disk(resource_name, fingerprints):
+    # TODO Take me out after testing
+    downloads = []
+    for fingerprint in fingerprints:
+        document_path = '%s/%s' % (resource_name, fingerprint, )
+        if os.path.exists(document_path):
+            document_file = open(document_path)
+            document_content = document_file.read()
+            document_file.close()
+            downloads.append(document_content)
+    return downloads
+
+def combine_downloads(downloads):
+    """
+    Combine an arbitrary number of downloaded Onionoo documents consisting
+    of four fields:
+     - relays_published (string): ignored here
+     - relays (array of objects): contains zero or more documents
+     - bridges_published (string): ignored here
+     - bridges (array of objects): contains zero or more documents
+    Return a single document with graphs containing data from all relays
+    and bridges.
+    """
+    documents = []
+    for download in downloads:
+        download_dict = json.loads(download)
+        documents.extend(download_dict['relays'])
+        documents.extend(download_dict['bridges'])
+    return combine_documents(documents)
+
+def combine_documents(documents):
+    """
+    Combine one or more relay or bridge documents into a single document
+    with the following fields:
+     - fingerprints (array of strings)
+     - (all combined graphs contained in relay or bridge documents)
+    """
+    fingerprints = []
+    graphs = {}
+    for document in documents:
+        for key, value in document.iteritems():
+            if key == 'fingerprint':
+                fingerprints.append(value)
+            elif key in graphs:
+                graphs[key].append(value)
+            else:
+                graphs[key] = [value]
+    combined_document = {}
+    combined_document['fingerprints'] = fingerprints
+    for key, value in graphs.iteritems():
+        combined_document[key] = combine_graphs(value)
+    return combined_document
+
+def combine_graphs(graphs):
+    """
+    Combine one or more sets of graphs into a single set of graphs.
+    """
+    histories = {}
+    for graph in graphs:
+        for key, value in graph.iteritems():
+            if key in histories:
+                histories[key].append(value)
+            else:
+                histories[key] = [value]
+    combined_graph = {}
+    for key, value in histories.iteritems():
+        combined_graph[key] = combine_histories(value)
+    return combined_graph
+
+def combine_histories(histories):
+    """
+    Combine an arbitrary number of graphs for a given time period into a
+    single graph.  The combined graph will range from max(first) to
+    max(last) of input graphs.
+    """
+    first, last, interval = [], [], []
+    values = {}
+    for history in histories:
+        first.append(history['first'])
+        last.append(history['last'])
+        interval.append(history['interval'])
+        factor = history['factor']
+        current = datetime.strptime(history['first'], '%Y-%m-%d %H:%M:%S')
+        for value in history['values']:
+            if value:
+                denormalized_value = value * history['factor']
+                if current in values:
+                    values[current].append(denormalized_value)
+                else:
+                    values[current] = [denormalized_value]
+            current = current + timedelta(seconds=history['interval'])
+    new_first = datetime.strptime(history['first'], '%Y-%m-%d %H:%M:%S')
+    new_last = datetime.strptime(history['last'], '%Y-%m-%d %H:%M:%S')
+    new_interval = max(interval)
+    new_current = new_first
+    new_denormalized_values = []
+    new_max = 0
+    while new_current <= new_last:
+        if new_current in values:
+            new_value = sum(values[new_current])
+            new_denormalized_values.append(new_value)
+            if new_value > new_max:
+                new_max = new_value
+        else:
+            new_denormalized_values.append(None)
+        new_current = new_current + timedelta(seconds=new_interval)
+    new_factor = new_max / 999.0
+    new_normalized_values = []
+    for value in new_denormalized_values:
+        if value:
+            new_normalized_values.append(int(value / new_factor))
+        else:
+            new_normalized_values.append(None)
+    combined_history = {}
+    combined_history['first'] = max(first)
+    combined_history['last'] = max(last)
+    combined_history['interval'] = new_interval
+    combined_history['factor'] = new_factor
+    combined_history['count'] = len(new_normalized_values)
+    combined_history['values'] = new_normalized_values
+    return combined_history
+
+def write_combined_document_to_disk(out_path, combined_document):
+    out_file = open(out_path, 'w')
+    out_file.write(json.dumps(combined_document))
+    out_file.close()
+
+if __name__ == '__main__':
+    main()
+
+
